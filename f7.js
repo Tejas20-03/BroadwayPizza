3a4c47715b152aeffe922002f41041fc3a64e0ce
Tejas20-03 <tejaschhabra.official@gmail.com>
2024-06-11 15:44:52 +0530

    initial commit

---
+++ f7.js
@@ -0,0 +1,12 @@
+// f7.js
+import Framework7 from "framework7";
+import Framework7React from "framework7-react";
+
+// Install Framework7 React Plugin
+Framework7.use(Framework7React);
+
+export default {
+  name: "My App",
+  theme: "auto",
+  routes: [],
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import "framework7/css/bundle";
+import "../styles/globals.css";
+import { App } from "framework7-react";
+import f7params from "../f7";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <App params={f7params}>
+      <Component {...pageProps} />
+    </App>
+  );
 }
+
+export default MyApp;
